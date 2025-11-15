2838c4ce3b5d9da06a72996d3ef84c2b12f2332d
yamsroun <yamsroun@gmail.com>
2025-11-15 11:09:46 +0900

    2.4. 회원 도메인 모델 작성(4) - 객체 생성 유효성 검증

--- src/main/java/jocture/joclearn/member/Member.java
+++ src/main/java/jocture/joclearn/member/Member.java
@@ -1,5 +1,6 @@
 package jocture.joclearn.member;
 
+import java.util.regex.Pattern;
 import lombok.Getter;
 
 //객체 불변성을 위해 필드에 private final을 선언하는게 좋다.
@@ -8,16 +9,44 @@ import lombok.Getter;
 @Getter
 public class Member {
 
+    /**
+     * 1. 닉네임 규칙과 이메일 규칙을 다른 곳에서도 사용해야 한다면 어떻게 해야할까? 중복 로직?
+     * 2. 닉네임, 이메일처럼 자체 검증 로직이 필요한 필드가 많아진다면? Member 객체의 책임이 크고 무거워진다.
+     * 3. 부가 로직 때문에 도메인 객체인 Member의 핵심 로직이 잘 보이지 않을 수 있다.
+     * - 이런 문제들을 어떻게 해결할 수 있을까?
+     */
+    private static final Pattern NICKNAME_PATTERN = Pattern.compile("^(?![._-])[A-Za-z0-9._-]{4,20}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+
     private String nickname;
     private String email;
     private String passwordHash;
     private MemberStatus status;
 
     private Member(String nickname, String email, String passwordHash, MemberStatus status) {
+        validateNickname(nickname);
         this.nickname = nickname;
+
+        validateEmail(email);
         this.email = email;
+
         this.passwordHash = passwordHash;
         this.status = status;
+    }
+
+    //validateXXX() - 유효성 검증 실패 시, Exception 발생; Return 타입은 void
+    //checkXXX() - validateXXX() 방식일 수도 있고, 결과를 Return 할 수도 있다.
+    // isValidXXX() - 유효성 검증 결과를 boolean 타입으로 Return 한다.
+    private void validateNickname(String nickname) {
+        if (!NICKNAME_PATTERN.matcher(nickname).matches()) {
+            throw new IllegalArgumentException("잘못된 닉네임 형식입니다.: " + nickname);
+        }
+    }
+
+    private static void validateEmail(String email) {
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new IllegalArgumentException("잘못된 이메일 형식입니다.: " + email);
+        }
     }
 
     public static Member create(String nickname, String email, String password, PasswordEncoder passwordEncoder) {
--- src/test/java/jocture/joclearn/member/MemberTest.java
+++ src/test/java/jocture/joclearn/member/MemberTest.java
@@ -2,8 +2,11 @@ package jocture.joclearn.member;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTest {
 
@@ -24,6 +27,26 @@ class MemberTest {
             }
         };
         this.member = Member.create("jjlim", "jjlim@abc.com", "abcd1234", passwordEncoder);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "JJ", "Lim", "123456789012345678901",
+        ".jocture", "_jocture", "-jocture",
+        "jocture*", "joc ture"
+    })
+    void create_wrongNickname(String nickname) {
+        assertThatThrownBy(() -> Member.create(nickname, "jjlim@abc.com", "abcd1234", passwordEncoder))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "jocture", "@abc.com", "j@a.c"
+    })
+    void create_wrongEmail(String email) {
+        assertThatThrownBy(() -> Member.create("jjlim", email, "abcd1234", passwordEncoder))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
