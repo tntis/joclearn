0914c764d9df9d391227ce3785d68eddcfb41281
tntis <tnbinsu@gmail.com>
2025-11-15 11:10:20 +0900

    2.4. 회원 도메인 모델 작성(4) - 객체 생성 유효성 검증

--- src/test/java/jocture/joclearn/member/MemberTest.java
+++ src/test/java/jocture/joclearn/member/MemberTest.java
@@ -2,8 +2,12 @@ package jocture.joclearn.member;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ValueSources;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTest {
 
@@ -23,7 +27,25 @@ class MemberTest {
                 return encode(rawPassword).equals(encodedPassword);
             }
         };
-        this.member = Member.create("jjlim", "jjlim@abc.com", "abcd1234", passwordEncoder);
+        this.member = Member.create("subin", "subin@qwe.com", "abcd1234", passwordEncoder);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings= {
+        "_ewdw","ssf","1234565678922345345"
+    })
+    void create_wrongNickname(String nickname ){
+        assertThatThrownBy(() -> Member.create(nickname,"subin@qwe.com","dd",passwordEncoder))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings= {
+        "subin","@qwe.com","sun@q"
+    })
+    void create_wrongEmail(String email) {
+        assertThatThrownBy(() -> Member.create("subin",email,"dd",passwordEncoder))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
